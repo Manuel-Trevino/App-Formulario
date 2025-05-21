e5b218eef30277a7d4eaf28f3cfb7dda5b28dbdc
Manuel Treviño <134456696+Manuel-Trevino@users.noreply.github.com>
2025-05-21 12:45:35 -0600

    Update config-paas.php

--- config-paas.php
+++ config-paas.php
@@ -1,13 +1,20 @@
 <?php
-$server = "20.10.1.4"; // IP privada de Azure Database
-$user = "azureuser";
-$password = "AzurePassword123!";
-$database = "appdb";
-$is_paas = true;
+$serverName = "ser-bd01.database.windows.net"; // Opcional: puedes especificar el puerto si no es el 1433 predeterminado
+$connectionOptions = array(
+    "Database" => "Bd01",
+    "Uid" => "admanuel",
+    "PWD" => "manuel123." // ¡Recuerda, esto debe ir en variables de entorno en producción!
+);
 
-$conn = new mysqli($server, $user, $password, $database);
+try {
+    // Establecer la conexión con PDO_SQLSRV
+    $conn = new PDO("sqlsrv:Server=$serverName;Database=" . $connectionOptions['Database'], $connectionOptions['Uid'], $connectionOptions['PWD']);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Habilita el modo de error de excepciones para un mejor manejo de errores
 
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    echo "Conexión exitosa a la base de datos SQL Server!";
+
+
+} catch (PDOException $e) {
+    die("Conexión fallida a SQL Server: " . $e->getMessage());
 }
 ?>
